0e33269ddb2a45e497e1abe850dce95c34924df6
Unknown <reo0527@naver.com>
2018-11-08 23:17:33 +0900

    일단 대략적으로 짜긴 했는데 맞는지는 몰겠음..
    필요한거 있으면 알려줘용<<<

--- OndemandCafe/Recipe.cpp
+++ OndemandCafe/Recipe.cpp
@@ -1,13 +1,20 @@
 #include "Recipe.h"
 
 
-
 Recipe::Recipe(const vector<pair<Ingredient *, Amount>>& recipeData) {
 	m_recipeData = recipeData;
 }
 
 void Recipe::setCoffeeName(const string m_nameOfCoffee) {
 	this->m_nameOfCoffee = m_nameOfCoffee;
+}
+
+string Recipe::getCoffeeName() const {
+	return m_nameOfCoffee;
+}
+
+int Recipe::getCoffeePrice() const {
+	return price;
 }
 
 Recipe Recipe::append(const pair<Ingredient *, Amount>& newIngredient)const {
@@ -24,14 +31,20 @@ Recipe Recipe::operator+(const pair<Ingredient *, Amount>& newIngredient)const {
 
 bool Recipe::operator==(const Recipe& recipeData) {
 
+
 }
 
-int Recipe::priceCalculate(const vector<pair<Ingredient *, Amount>>& recipeData) {
-	int retPrice = 0;
+const bool Recipe::equals(const Recipe& recipeData) const {
+
+
+
+}
+
+void Recipe::priceCalculate(const vector<pair<Ingredient *, Amount>>& recipeData) {
+	price = 0;
 	for (auto i : recipeData) {
-		retPrice += getUnitPrice(i.first)*(i.second);
+		 price+= getUnitPrice(i.first)*(i.second);
 	}
-	return retPrice;
 }
 
 
--- OndemandCafe/Recipe.h
+++ OndemandCafe/Recipe.h
@@ -17,14 +17,18 @@ class Recipe{
 private:
 	string m_nameOfCoffee;
 	vector< pair<Ingredient *, Amount>>m_recipeData;
+	int price;
 public:
 	
 	Recipe(const vector<pair<Ingredient *, Amount>>& recipeData);
 	void setCoffeeName(const string m_nameOfCoffee);
+	string getCoffeeName() const;
+	int getCoffeePrice() const;
 	Recipe append(const pair<Ingredient *, Amount>& newIngredient)const;
 	Recipe operator+(const pair<Ingredient *, Amount> &newIngredient)const;
 	bool operator==(const Recipe& recipeData);
-	int priceCalculate(const vector<pair<Ingredient *, Amount>>& recipeData);
+	const bool equals(const Recipe& recipeData)const;
+	void priceCalculate(const vector<pair<Ingredient *, Amount>>& recipeData);
 
 	
 };
